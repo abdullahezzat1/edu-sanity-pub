390c5588294da54a0808c51fe3ee0bbcceda8b4c
Abdullah Noureldeen <abdullahezzat3@gmail.com>
2023-09-11 12:46:02 +0300

    task 1

---
+++ frontend/src/views/Task1.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react'
+import sanityClient from '../Client'
+
+function renderPetListItem(pet) {
+  const { name } = pet;
+  return (
+    <li>{name}</li>
+  );
+}
+
+function Task1() {
+  const [pets, setPets] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "pet"]{
+      name,
+      title,
+      fields{
+        name,
+        type,
+        title
+      }
+    }`,
+      )
+      .then((data) => setPets(data))
+      .catch(console.error)
+  }, []);
+
+  return (
+    <>
+      <h1>Most common types of Pets</h1>
+      <ul> {pets && pets.map(renderPetListItem)} </ul>
+    </>
+  )
+}
+
+export default Task1
